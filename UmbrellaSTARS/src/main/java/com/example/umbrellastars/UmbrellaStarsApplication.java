1615e26427d11329b17634553f023dc36880894b
CristianFernan <cristianfernandoparadamira@gmail.com>
2026-04-22 14:22:52 -0600

    feat: added report system that prints the results of filters

--- UmbrellaSTARS/src/main/java/com/example/umbrellastars/UmbrellaStarsApplication.java
+++ UmbrellaSTARS/src/main/java/com/example/umbrellastars/UmbrellaStarsApplication.java
@@ -1,7 +1,10 @@
 package com.example.umbrellastars;
 
+import com.example.umbrellastars.services.BioWarningService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UmbrellaStarsApplication {
@@ -10,4 +13,26 @@ public class UmbrellaStarsApplication {
         SpringApplication.run(UmbrellaStarsApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner run (BioWarningService bioWarningService) {
+      return args -> {
+          System.out.println("Starting App...");
+          System.out.println("======All BOWs=====");
+          bioWarningService.findAll().forEach(bow ->
+                  System.out.println("[S.T.A.R.S-REPORT] Nombre: " + bow.getName() +" | Nivel de Peligro:" + bow.getRiskLevel() + " | Punto Débil: " + bow.getWeakPoint()));
+          // Filtrados
+          // por virus base
+          System.out.println("=====BOWs by base Virus====");
+          bioWarningService.findByBaseVirus("G-virus").forEach(bow ->
+                  System.out.println("[S.T.A.R.S-REPORT] Nombre: " + bow.getName() +" | Nivel de Peligro:" + bow.getRiskLevel() + " | Punto Débil: " + bow.getWeakPoint()));
+          // por estado actual
+          System.out.println("=====BOWs by their current state====");
+          bioWarningService.findByCurrentState("Eliminated").forEach(bow ->
+                  System.out.println("[S.T.A.R.S-REPORT] Nombre: " + bow.getName() +" | Nivel de Peligro:" + bow.getRiskLevel() + " | Punto Débil: " + bow.getWeakPoint()));
+          // los que son activos, sin repetir
+          System.out.println("=====Free Distinct BOWs====");
+          bioWarningService.findDistinctVirus().forEach(bow ->
+                  System.out.println("[S.T.A.R.S-REPORT] Nombre: " + bow.getName() +" | Nivel de Peligro:" + bow.getRiskLevel() + " | Punto Débil: " + bow.getWeakPoint()));
+      };
+    };
 }
