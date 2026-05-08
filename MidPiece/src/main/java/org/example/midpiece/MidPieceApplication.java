eeb68e3358c55ec530932445d685c622e5d8e04a
CristianFernan <cristianfernandoparadamira@gmail.com>
2026-05-08 17:16:01 -0600

    feat: created the one piece project

---
+++ MidPiece/src/main/java/org/example/midpiece/MidPieceApplication.java
@@ -0,0 +1,13 @@
+package org.example.midpiece;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidPieceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MidPieceApplication.class, args);
+    }
+
+}
---
+++ MidPiece/src/test/java/org/example/midpiece/MidPieceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.midpiece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidPieceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
