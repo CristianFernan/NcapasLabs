f3bc00df26b8da089f15794cb81c3b82b16cc368
CristianFernan <cristianfernandoparadamira@gmail.com>
2026-06-13 19:54:22 -0600

    feat: created backend project

---
+++ lab-4/lab-4-backend/src/test/java/org/example/lab4backend/Lab4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lab4backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
